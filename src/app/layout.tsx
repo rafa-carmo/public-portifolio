12d5591185e6cf0b873392b340d3de4c21354a5f
Rafael do Carmo <rafaelcarmo143@gmail.com>
2026-08-06 16:01:46 -0300

    Remove Google tag comment from layout
    Removed Google tag comment from layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,6 @@ export default async function LocaleLayout({
   return (
     <html lang={locale ?? "en"}>
       <head>
-        <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-D484CQFXMX"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
